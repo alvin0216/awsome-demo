24c31b70bcbb24df5123fe08ba21f9984366f2d6
guosw <guoshaowei@pvmedtech.com>
2018-10-18 16:48:46 +0800

    upload takeLatest

---
+++ react/redux-saga/3-takeLatest/redux/actions.js
@@ -0,0 +1,11 @@
+export const increment = () => ({
+  type: 'INCREMENT'
+})
+
+export const incrementAsync = () => ({
+  type: 'INCREMENT_ASYNC'
+})
+
+export const fetchUser = () => ({
+  type: 'FETCH_USER_REQUEST'
+})
